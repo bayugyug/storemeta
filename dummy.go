d7d4fe681dd6be48c0e0d9317f2ecba641d57a50
bayugyug <mtejero@artofclick.com>
2016-09-30 15:13:10 +0800

    .

---
+++ dummy.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func A() {
+	fmt.Println("test a")
+}
+
+func AB() {
+	fmt.Println("test ab")
+}
---
+++ dummy_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	t.Log("test a")
+	os.Exit(0)
+}
